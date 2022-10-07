64a60a99bce6ce1d3fb9bbbc082a49dbf19bca30
Ashish Yadav <unixm98@gmail.com>
2022-10-07 07:03:11 +0530

    routine commit

--- Python/Web Scraping/RestNames.py
+++ Python/Web Scraping/RestNames.py
@@ -1,15 +1,13 @@
-import requests
+import requests,random
 from bs4 import BeautifulSoup
-r = requests.get('https://news.google.com/topstories?hl=en-IN&gl=IN&ceid=IN:en')
+r = requests.get('https://budgetbranders.com/blog/creative-indian-restaurant-names/')
 soup = BeautifulSoup(r.content, 'html.parser')
-content = soup.find_all('h3', class_ = 'ipQwMb ekueJc RD0gLb')
+s = soup.find('ol')
+content = s.find_all('span')
+str = "INSERT INTO RESTAURANT_DIM VALUES('{}','{}',{},'{}','{}');"
+ids =0
+for ids in range(0,9):
+    ids += random.randint(0,9)
+print(ids)
 for cont in content:
-    print(cont.text)
-# r = requests.get('https://budgetbranders.com/blog/creative-indian-restaurant-names/')
-# soup = BeautifulSoup(r.content, 'html.parser')
-# s = soup.find('ol')
-# content = s.find_all('span')
-# str = "INSERT INTO EMPLOYEE VALUES('{}','B','smith',123456789,to_date('1965-01-09','yyyy-mm-dd'),'731 Fondren Houston,TX','M',30000,333445555,5);"
-# for cont in content:
-#     print(str.format(cont.text))
-
+    print(str.format(cont.text,cont.text,cont.text,cont.text,cont.text))
