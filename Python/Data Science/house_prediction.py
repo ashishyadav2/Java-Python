7e832829c011ff04b6163460ebbef783a07cb4d1
ashishyadav2 <unixm98@gmail.com>
2023-06-16 00:11:23 +0530

    update house prediction.ipynb

--- Python/Data Science/house_prediction.py
+++ Python/Data Science/house_prediction.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pickle as pkl
 import numpy as np
 import locale
+import math
+import time
 
 model = pkl.load(open('house_model.pkl', 'rb'))
 df = pkl.load(open('dataframe.pkl', 'rb'))
@@ -13,7 +15,15 @@ bedroom = st.number_input("Bedroom", step=1)
 locale.setlocale(locale.LC_ALL, 'en_IN')
 
 if st.button("Predict Price"):
+    with st.spinner('Calculating'):
+        time.sleep(5)
+    var = None
     query = np.array([[area, location, bedroom]])
-    prediction = np.exp(model.predict(query)[0])
-    formatted_price = locale.currency(prediction, grouping=True)
-    st.title(formatted_price)
+    prediction = math.floor(np.exp(model.predict(query)[0]))
+    lower_bound = prediction - ((prediction*15)/100)
+    upper_bound = prediction + ((prediction*15)/100)
+    f1 = locale.currency(lower_bound, grouping=True)
+    f2 = locale.currency(upper_bound, grouping=True)
+    avg = locale.currency(prediction, grouping=True)
+    st.markdown(f"Average: {str(avg)}")    
+    st.header(f1+" - "+f2)
